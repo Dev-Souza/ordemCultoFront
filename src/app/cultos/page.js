9b5a6126c7134e01d59ef6d7edf67d60767af3ee
Dev-Souza <kauan.souza.dev@gmail.com>
2025-05-12 12:48:59 -0300

    feat: function the delete culto

--- src/app/cultos/page.js
+++ src/app/cultos/page.js
@@ -10,12 +10,15 @@ export default function Page() {
     const [cultos, setCultos] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [token, setToken] = useState(null);
 
     const router = useRouter();
 
     useEffect(() => {
         // Pegando o token do localStorage
         const authToken = localStorage.getItem('authToken');
+        setToken(authToken)
+
         async function carregarCultos() {
             try {
                 const resposta = await ordemCulto.get('culto', {
@@ -35,6 +38,29 @@ export default function Page() {
         carregarCultos();
     }, []);
 
+    async function deletarCulto(id) {
+        try {
+            if (!confirm('Deseja realmente excluir?')) return;
+            const resposta = await ordemCulto.delete(`culto/${id}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`  // Enviando o token no cabeçalho
+                }
+            });
+            alert('Culto excluído com sucesso!')
+            router.push("/cultos")
+        } catch (error) {
+            if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+                alert("Sessão expirada, por favor faça login novamente.");
+                router.push("/login");
+            } else {
+                setError(error);
+                alert("Erro ao excluir culto.");
+            }
+        } finally {
+            setLoading(false); // Marca como carregado
+        }
+    }
+
     if (loading) {
         return <Spinners />; // Chamando o component
     }
@@ -45,8 +71,9 @@ export default function Page() {
             <ul>
                 {cultos.map(culto => (
                     <React.Fragment key={culto.id}>
-                        <li>{culto.tituloCulto}</li> 
+                        <li>{culto.tituloCulto}</li>
                         <Link href={`cultos/forms/${culto.id}`}>Editar</Link>
+                        <button onClick={() => deletarCulto(culto.id)}>Deletar</button>
                     </React.Fragment>
                 ))}
             </ul>
