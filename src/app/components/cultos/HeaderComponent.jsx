c19c5a7049c76b79cf248657b9c4bd060541dd03
Dev-Souza <kauan.souza.dev@gmail.com>
2025-05-13 11:39:58 -0300

    feat: create header component

---
+++ src/app/components/cultos/HeaderComponent.jsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+function HeaderComponent(props) {
+  return (
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/cultos">MAVA</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <Link href={`/${props.caminho}`}>Voltar</Link>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default HeaderComponent;
