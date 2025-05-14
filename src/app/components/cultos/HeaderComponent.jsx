2c25759d3c777dce91b776d866e39f83a8555551
Dev-Souza <kauan.souza.dev@gmail.com>
2025-05-14 11:56:05 -0300

    style: updated the header

--- src/app/components/cultos/HeaderComponent.jsx
+++ src/app/components/cultos/HeaderComponent.jsx
@@ -4,13 +4,13 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function HeaderComponent(props) {
   return (
-    <Navbar className="bg-white py-3">
+    <Navbar className="bg-white py-3 fixed-top" >
       <Container>
         <Navbar.Brand href="/cultos">MAVA</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            <Link href={`/${props.caminho}`}>Voltar</Link>
+            <Link href={`/${props.caminho}`} className='btn btn-danger text-white'>Voltar</Link>
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
