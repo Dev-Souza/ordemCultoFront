31865b13667849dba982e9bf44df725c4e4f09d3
Dev-Souza <kauan.souza.dev@gmail.com>
2025-05-20 11:29:18 -0300

    fix: loading center

--- src/app/components/Spinners.js
+++ src/app/components/Spinners.js
@@ -2,9 +2,18 @@ import Spinner from 'react-bootstrap/Spinner';
 
 function Spinners() {
   return (
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
   );
 }
 
