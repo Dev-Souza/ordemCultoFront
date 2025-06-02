17c2e4cccb5a2541be02dcc4c66547632459eb30
Dev-Souza <kauan.souza.dev@gmail.com>
2025-06-02 12:27:44 -0300

    update: baseURL

--- src/app/services/ordemCulto.js
+++ src/app/services/ordemCulto.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const ordemCulto = axios.create({
-    baseURL: 'http://localhost:8080/'
+    baseURL: process.env.CHAVE_API
 })
 
 export default ordemCulto
